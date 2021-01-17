871455c52d3de976ebab8681564b1d6df6fd16f7
J Scott Rumptz <jscott@rumptz.tech>
2021-01-17 00:40:26 -0600

    uv api complete

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -50,15 +50,37 @@ let citySearchHistory = function (city) {
 };
 
 let displayWeather = function(weatherData) {
+
+    // format and display the values
     $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(` <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
     $("#main-city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
     $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + "%");
     $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
 
     // uv api call
-    // fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon="+ weatherData.coord.lon + "&appid=ce39e7239416ad754359ca762d28521a")
+    fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon="+ weatherData.coord.lon + "&appid=ce39e7239416ad754359ca762d28521a")
+        .then(function(response) {
+            response.json().then(function(data) {
 
-    $("#main-city-uv").text("UV Index: ");
+                // display the uv index value
+                $("#uv-box").text(data.value);
+                console.log(data);
+
+                // highlight the value using the EPA's UV Index Scale colors
+                if(data.value >= 11) {
+                    $("#uv-box").css("background-color", "#6c49cb")
+                } else if (data.value < 11 && data.value >= 8) {
+                    $("#uv-box").css("background-color", "#d90011")
+                } else if (data.value < 8 && data.value >= 6) {
+                    $("#uv-box").css("background-color", "#f95901")
+                } else if (data.value < 6 && data.value >= 3) {
+                    $("#uv-box").css("background-color", "#f7e401")
+                } else {
+                    $("#uv-box").css("background-color", "#299501")
+                }
+                
+        })
+    })
 };
 
 // event handlers
