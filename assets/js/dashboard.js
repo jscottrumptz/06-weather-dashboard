6c0fbfacd26ccb488f841c13c806d3542efb1006
J Scott Rumptz <jscott@rumptz.tech>
2021-01-17 00:05:30 -0600

    main simple

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,10 +1,9 @@
-let searchformEl = document.querySelector("#search-form");
-let searchInputEl = document.querySelector("#cityname");
+// let searchInputEl = document.querySelector("#cityname");
 let prevSearchListEl = document.querySelector("#search-history");
 
 let getCityWeather = function(city) {
     // format the OpenWeather api url
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ce39e7239416ad754359ca762d28521a";
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ce39e7239416ad754359ca762d28521a&units=imperial";
 
     // make a request to the url
     fetch(apiUrl)
@@ -13,7 +12,7 @@ let getCityWeather = function(city) {
         // request was successful
             if (response.ok) {
                 response.json().then(function(data) {
-                    // displayWeather(data, city);
+                    displayWeather(data);
                     console.log(data);
                 });
             } else {
@@ -21,7 +20,7 @@ let getCityWeather = function(city) {
             }
         })  
 
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        // alert user if there is no responce from OpenWeather
         .catch(function(error) {
             alert("Unable to connect to OpenWeather");
         })
@@ -32,20 +31,35 @@ let searchSubmitHandler = function(event) {
     event.preventDefault();
 
     // get value from input element
-    let cityName = searchInputEl.value.trim();
+    let cityName = $("#cityname").val().trim();
 
-    // insert cityname into the api url bay calling the getCityWeather function
+    // check if the search field has a value
     if(cityName) {
+        // pass the value to getCityWeather function
         getCityWeather(cityName);
-        searchInputEl.value = "";
+        // clear the search input
+        $("#cityname").val("");
     } else {
+        // if nothing was entered alert the user
         alert("Please enter a city name");
     }
 };
 
-let displayWeather = function(weatherData, citySearch) {
+let citySearchHistory = function (city) {
 
-}
+};
 
+let displayWeather = function(weatherData) {
+    $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(` <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+    $("#main-city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+    $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + "%");
+    $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
 
-searchformEl.addEventListener("submit", searchSubmitHandler);
+    // uv api call
+    // fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon="+ weatherData.coord.lon + "&appid=ce39e7239416ad754359ca762d28521a")
+
+    $("#main-city-uv").text("UV Index: ");
+};
+
+// event handlers
+$("#search-form").submit(searchSubmitHandler)
