2e93a49405c5ad22e63d280cbb3f2715d319213e
J Scott Rumptz <jscott@rumptz.tech>
2021-01-19 16:35:58 -0600

    fixed https errors

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -95,7 +95,7 @@ let displayWeather = function(weatherData) {
                     <div class="col-md-2 m-2 py-3 card text-white bg-primary">
                         <div class="card-body p-1">
                             <h5 class="card-title">` + dayjs(data.list[i].dt * 1000).format("MM/DD/YYYY") + `</h5>
-                            <img src="http://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="rain">
+                            <img src="https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="rain">
                             <p class="card-text">Temp: ` + data.list[i].main.temp + `</p>
                             <p class="card-text">Humidity: ` + data.list[i].main.humidity + `</p>
                         </div>
